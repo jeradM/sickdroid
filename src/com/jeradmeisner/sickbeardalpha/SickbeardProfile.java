14779c46f5678fcfc1b98fdfd24c9c0046c3880c
Jerad Meisner <jerad.meisner@gmail.com>
2013-06-12 18:37:09 -0700

    added profiles class. can read/write profile info to SP

--- src/com/jeradmeisner/sickbeardalpha/SickbeardProfile.java
+++ src/com/jeradmeisner/sickbeardalpha/SickbeardProfile.java
@@ -7,20 +7,16 @@ import android.preference.PreferenceManager;
 
 public class SickbeardProfile {
 
-    public static final String PREFS_HOST = "host";
-    public static final String PREFS_PORT = "port";
-    public static final String PREFS_WEBROOT = "webroot";
-    public static final String PREFS_APIKEY = "apikey";
-    public static final String PREFS_USEHTTPS = "https";
-
+    private String name;
     private String host;
     private String port;
     private String webroot;
     private String apikey;
     private boolean useHttps;
 
-    public SickbeardProfile(String host, String port, String webroot, String apikey, boolean useHttps)
+    public SickbeardProfile(String name, String host, String port, String webroot, String apikey, boolean useHttps)
     {
+        this.name = name;
         this.host = host;
         this.port = port;
         this.webroot = webroot;
@@ -28,24 +24,31 @@ public class SickbeardProfile {
         this.useHttps = useHttps;
     }
 
-    public SickbeardProfile(SharedPreferences prefs)
+    public SickbeardProfile(String name, SharedPreferences prefs)
     {
-        host = prefs.getString(PREFS_HOST, "127.0.0.1");
-        port = prefs.getString(PREFS_PORT, "8080");
-        webroot = prefs.getString(PREFS_WEBROOT, "");
-        apikey = prefs.getString(PREFS_APIKEY, "12345");
-        useHttps = prefs.getBoolean(PREFS_USEHTTPS, false);
+        this.name = name;
+        host = prefs.getString(SickbeardProfiles.PREFS_HOST, "127.0.0.1");
+        port = prefs.getString(SickbeardProfiles.PREFS_PORT, "8080");
+        webroot = prefs.getString(SickbeardProfiles.PREFS_WEBROOT, "");
+        apikey = prefs.getString(SickbeardProfiles.PREFS_APIKEY, "12345");
+        useHttps = prefs.getBoolean(SickbeardProfiles.PREFS_USEHTTPS, false);
     }
 
-    public void loadProfile(Context context)
+    public void setProfile(Context context)
     {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PREFS_HOST, host);
-        editor.putString(PREFS_PORT, port);
-        editor.putString(PREFS_WEBROOT, webroot);
-        editor.putString(PREFS_APIKEY, apikey);
-        editor.putBoolean(PREFS_USEHTTPS, useHttps);
+        editor.putString(SickbeardProfiles.PREFS_HOST, host);
+        editor.putString(SickbeardProfiles.PREFS_PORT, port);
+        editor.putString(SickbeardProfiles.PREFS_WEBROOT, webroot);
+        editor.putString(SickbeardProfiles.PREFS_APIKEY, apikey);
+        editor.putBoolean(SickbeardProfiles.PREFS_USEHTTPS, useHttps);
+        editor.putString(SickbeardProfiles.PREFS_CURRENT_PROFILE, name);
         editor.commit();
+    }
+
+    public String getName()
+    {
+        return name;
     }
 }
---
+++ src/com/jeradmeisner/sickbeardalpha/SickbeardProfiles.java
@@ -0,0 +1,90 @@
+package com.jeradmeisner.sickbeardalpha;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Celestina
+ * Date: 6/12/13
+ * Time: 5:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SickbeardProfiles {
+
+    private static SickbeardProfiles instance;
+
+    public static final String PREFS_HOST = "host";
+    public static final String PREFS_PORT = "port";
+    public static final String PREFS_WEBROOT = "webroot";
+    public static final String PREFS_APIKEY = "apikey";
+    public static final String PREFS_USEHTTPS = "https";
+
+    public static final String PREFS_CURRENT_PROFILE = "current_profile";
+    public static final String PREFS_PROFILES = "profiles";
+
+    private List<SickbeardProfile> profiles;
+
+    public static SickbeardProfiles getInstance()
+    {
+        if (instance == null)
+            instance = new SickbeardProfiles();
+
+        return instance;
+    }
+
+    private SickbeardProfiles()
+    {
+    }
+
+    public void loadProfiles(Context ctx)
+    {
+        ctx = ctx.getApplicationContext();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+        String profileString = prefs.getString(PREFS_PROFILES, null);
+        profiles = new ArrayList<SickbeardProfile>();
+
+        if (profileString != null) {
+            String[] profileArray = profileString.split(":");
+
+            for (String name : profileArray) {
+                SharedPreferences pref = ctx.getSharedPreferences(name, Context.MODE_PRIVATE);
+                SickbeardProfile profile = new SickbeardProfile(name, pref);
+                profiles.add(profile);
+            }
+        }
+    }
+
+    public void writeProfiles(Context ctx)
+    {
+        ctx = ctx.getApplicationContext();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+        StringBuilder sb = new StringBuilder();
+        for (SickbeardProfile profile : profiles) {
+            sb.append(profile.getName() + ":");
+        }
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PREFS_PROFILES, sb.toString());
+        editor.commit();
+    }
+
+    public void addProfile(String name, String host, String port,
+                           String webroot, String apikey, boolean https)
+    {
+        SickbeardProfile profile = new SickbeardProfile(name, host, port, webroot, apikey, https);
+        profiles.add(profile);
+    }
+
+    public List<SickbeardProfile> getProfiles()
+    {
+        return profiles;
+    }
+
+
+}
