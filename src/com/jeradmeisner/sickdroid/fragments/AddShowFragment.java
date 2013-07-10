d30cdf9ba90a8875f5665dcfde588c6617b21786
Jerad Meisner <jerad.meisner707@gmail.com>
2013-07-09 20:57:30 -0700

    changed spinners to use string-array resources.

--- src/com/jeradmeisner/sickdroid/fragments/AddShowFragment.java
+++ src/com/jeradmeisner/sickdroid/fragments/AddShowFragment.java
@@ -2,6 +2,7 @@ package com.jeradmeisner.sickdroid.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -40,22 +41,35 @@ public class AddShowFragment extends SherlockDialogFragment {
         TextView titleView = (TextView)view.findViewById(R.id.fragment_title);
         titleView.setText(result.toString());
 
-        Spinner quality = (Spinner)view.findViewById(R.id.quality_spinner);
-        Spinner status = (Spinner)view.findViewById(R.id.status_spinner);
-        CheckBox flatten = (CheckBox)view.findViewById(R.id.flatten_folders);
+        final Spinner quality = (Spinner)view.findViewById(R.id.quality_spinner);
+        final Spinner status = (Spinner)view.findViewById(R.id.status_spinner);
+        final CheckBox flatten = (CheckBox)view.findViewById(R.id.flatten_folders);
 
-        String[] qualityList = {"SD", "HD720", "HD1080", "Any"};
-        ArrayAdapter<String> qAdapter = new ArrayAdapter<String>(getSherlockActivity(), android.R.layout.simple_list_item_1, qualityList);
+        //String[] qualityList = {"SD", "HD720", "HD1080", "Any"};
+        ArrayAdapter<CharSequence> qAdapter = ArrayAdapter.createFromResource(getSherlockActivity(), R.array.quality_strings, android.R.layout.simple_spinner_item);
+        qAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         quality.setAdapter(qAdapter);
 
-        String[] statusList = {"Skipped", "Wanted", "Ignored"};
-        ArrayAdapter<String> sAdapter = new ArrayAdapter<String>(getSherlockActivity(), android.R.layout.simple_list_item_1, statusList);
+
+        //String[] statusList = {"Skipped", "Wanted", "Ignored"};
+        ArrayAdapter<CharSequence> sAdapter = ArrayAdapter.createFromResource(getSherlockActivity(), R.array.status_strings, android.R.layout.simple_spinner_item);
+        sAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         status.setAdapter(sAdapter);
 
         setStyle(SherlockDialogFragment.STYLE_NO_TITLE, R.style.Theme_Sickdroid_LightGreen);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getSherlockActivity());
-        builder.setView(view);
+        builder.setView(view)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String stat = String.valueOf(status.getSelectedItem());
+                        String qual = String.valueOf(quality.getSelectedItem());
+                        boolean flat = flatten.isChecked();
+                        Toast.makeText(getSherlockActivity(), stat + " " + qual + " " + String.valueOf(flat), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", null);
 
         return builder.create();
     }
